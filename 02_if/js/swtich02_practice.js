40f4685212938858155244afc63bd0afe98785fa
towa0105 <2250290@ecc.ac.jp>
2025-12-14 23:58:23 +0900

    追加：swtich02途中

--- 02_if/js/swtich02_practice.js
+++ 02_if/js/swtich02_practice.js
@@ -3,20 +3,39 @@ const num2 = 2;
 const calc = "a";
 let ans = 0;
 
-if(calc == "a"){
+if (calc == "a") {
   ans = num1 + num2;
 }
-else if(calc == "b"){
+else if (calc == "b") {
   ans = num1 - num2;
 }
-else if(calc == "c"){
+else if (calc == "c") {
   ans = num1 * num2;
 }
-else if(calc == "d"){
+else if (calc == "d") {
   ans = num1 / num2;
 }
-else{
+else {
   ans = "error!";
 }
 
 console.log(ans);
+
+
+switch (ans) {
+  case "a":
+    console.log("拡張子は.cssです");
+    break;
+  case "b":
+    console.log("拡張子は.htmlです");
+    break;
+  case "c":
+    console.log(num1 * num2)
+    break;
+  case "d":
+    console.log(num1 / num2)
+    break;
+  default:
+    console.log("error!");
+    break;
+}
