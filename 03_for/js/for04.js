8b70e01bee805635591b805617e1df0cbc6be67d
towa0105 <2250290@ecc.ac.jp>
2025-12-18 22:34:09 +0900

    追加：for04完了

--- 03_for/js/for04.js
+++ 03_for/js/for04.js
@@ -1,14 +1,9 @@
 let num = prompt("回数を入力")
 num = Number(num);
 
-for (let i = 1; i < 10; i++) {
+for (let i = 0; i < num; i++) {
     const num_1 = i + 1;
     console.log(num_1 + "回目の表示")
 }
 
 
-
-
-// for (let i = 5; i < 12; i++) {
-//     console.log(i + "回目の表示")
-// }
