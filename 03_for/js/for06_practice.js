7afa87a860ff597588461b48788da19599e69798
towa0105 <2250290@ecc.ac.jp>
2025-12-19 23:56:14 +0900

    追加：for06途中

--- 03_for/js/for06_practice.js
+++ 03_for/js/for06_practice.js
@@ -2,3 +2,11 @@
 let wallet = 12800;
 //価格
 let price = 980;
+
+if (wallet >= 51) {
+    console.log(over_num)
+}
+else for (let i = 0; i < num; i++) {
+    const num = i + 1;
+    console.log(num + "回目の表示")
+}
