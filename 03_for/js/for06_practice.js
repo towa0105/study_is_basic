95d361f5ba10dd2e2ff5a92274102e0f8fe380b7
towa0105 <2250290@ecc.ac.jp>
2025-12-20 20:19:59 +0900

    追加：for06途中

--- 03_for/js/for06_practice.js
+++ 03_for/js/for06_practice.js
@@ -1,12 +1,42 @@
 //残金
 let wallet = 12800;
-//価格
+// //価格
 let price = 980;
 
-if (wallet >= 51) {
-    console.log(over_num)
+// for(i<)
+
+// let afterprice = wallet - price;
+// console.log(afterprice)
+
+
+for (let i = 0; i < num; i++) {
+    const num_1 = i + 1;
+    console.log(num_1 + "回目の表示")
 }
-else for (let i = 0; i < num; i++) {
-    const num = i + 1;
-    console.log(num + "回目の表示")
-}
+
+
+
+
+let money = 12800;
+for (let i = 0; )
+
+    while (true) {
+        let price;
+
+        // 5回目以降は割引
+        if (i + 1 >= 5) {
+            price = 750;
+        } else {
+            price = 980;
+        }
+
+        // 所持金が足りるか判定
+        if (wallet >= price) {
+            wallet -= price;
+            i++;
+            console.log(`${i}回目の買い物成功。残金は${wallet}円`);
+        } else {
+            console.log(`買い物終了${i}個買えた`);
+            break;
+        }
+    }
