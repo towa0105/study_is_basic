71ea01bcc34d7ec57f164401775882d85479afab
towa0105 <2250290@ecc.ac.jp>
2025-12-24 23:28:40 +0900

    追加：array01完了

---
+++ 04_array/js/array01.js
@@ -0,0 +1,12 @@
+let fruits1 = new Array();
+console.log(fruits1);
+
+fruits1[0] = "apple";
+fruits1[1] = "banana";
+fruits1[2] = "lemon";
+console.log(fruits1);
+
+let fruits2 = [];
+// [] に直接データを入れる場合、 , カンマを使って区切ります。
+fruits2 = ["apple", "banana", "lemon"];
+console.log(fruits2);
