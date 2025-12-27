fd674e26c5886e52fc6770f99ae4d9353ab96c91
towa0105 <2250290@ecc.ac.jp>
2025-12-27 22:33:21 +0900

    追加：array02完了

--- 04_array/js/array02.js
+++ 04_array/js/array02.js
@@ -1,4 +1,5 @@
 let ramen = [];
 // [] に直接データを入れる場合、 , カンマを使って区切ります。
-ramen = ["sio", "tonkotu", "nibosi", "shoyu", "jiro"];
+ramen = ["sio", "miso", "tonkotu", "nibosi", "shoyu", "jiro"];
 console.log(ramen);
+console.log(`ここのお店のラーメンは全部で${ramen.length}種類、おすすめは${ramen[1]}です`)
