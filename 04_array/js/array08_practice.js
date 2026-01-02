8ae1bbafde1322a3c9aab61e83ffbf639865c00b
towa0105 <2250290@ecc.ac.jp>
2026-01-03 02:56:34 +0900

    追加：array08途中

--- 04_array/js/array08_practice.js
+++ 04_array/js/array08_practice.js
@@ -1,2 +1,7 @@
 // 画面を更新する度に0-5までのランダムな値が入る
-const no = Math.floor(Math.random()*5);
+const no = Math.floor(Math.random() * 5);
+console.log(no)
+
+let omikuji = ["大吉ですおめでとう！", "滝山", "武内", "桃山", "板倉"];
+
+console.log(`${omikuji}です`)
