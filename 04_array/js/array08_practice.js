9f953788d431c8e8351146a7bf2200edd4f60103
towa0105 <2250290@ecc.ac.jp>
2026-01-06 02:10:17 +0900

    追加：array08完了

--- 04_array/js/array08_practice.js
+++ 04_array/js/array08_practice.js
@@ -2,6 +2,5 @@
 const no = Math.floor(Math.random() * 5);
 console.log(no)
 
-let omikuji = ["大吉ですおめでとう！", "滝山", "武内", "桃山", "板倉"];
-
-console.log(`${omikuji}です`)
+let omikuji = ["大吉ですおめでとう！", "中吉ですラッキー！", "コメントに困る吉です", "小吉です", "凶ですアンラッキー", "大凶です残念・・・"];
+console.log(`${omikuji[no]}です`)
