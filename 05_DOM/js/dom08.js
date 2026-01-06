20890316825e1ed5213ca6ba3f18e0df01a312c7
towa0105 <2250290@ecc.ac.jp>
2026-01-07 00:08:55 +0900

    追加：dom08途中

---
+++ 05_DOM/js/dom08.js
@@ -0,0 +1,10 @@
+const btnL = document.getElementById("btnL");
+const btnS = document.getElementById("btnS");
+const text = document.getElementById("text")
+
+btnL.onclick = () => {
+    document.body.style.fontSize = "80px";
+}
+btnS.onclick = () => {
+    document.body.style.fontSize = "20px";
+}
