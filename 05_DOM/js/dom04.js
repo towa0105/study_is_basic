6ce42e4367f365e3c6329ce5550dc62670327276
towa0105 <2250290@ecc.ac.jp>
2026-01-06 19:02:04 +0900

    追加：dom04完了

--- 05_DOM/js/dom04.js
+++ 05_DOM/js/dom04.js
@@ -1,2 +1,7 @@
 const text = document.getElementById("school")
 const btn = document.getElementById("btn")
+const output = document.getElementById("text")
+
+btn.onclick = () => {
+    output.innerText = text.value;
+}
