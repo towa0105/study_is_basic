acbf407538a3c8bd92dc4cacde95c064c8c15627
towa0105 <2250290@ecc.ac.jp>
2026-01-08 22:38:34 +0900

    追加：dom10途中

---
+++ 05_DOM/js/dom10.js
@@ -0,0 +1,14 @@
+// // const li = document.querySelectorAll("tbody>tr");
+// const list = document.getElementsByClassName("name");
+// console.log(list[0]);
+
+// // innerHTML
+
+// // console.log(li[0].name);
+
+// // 全件取得したい場合は、for文で取得
+// // 要素数は lengthで取得できるので下記で取得
+// for (let i = 0; i < list.length; i++) {
+//     console.log(list[i].textContent);
+// }
+
