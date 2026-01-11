810bcafb07b35e881435dfa8582e93aa2cbee0d2
towa0105 <2250290@ecc.ac.jp>
2026-01-11 15:37:09 +0900

    追加：dom11途中

--- 05_DOM/js/dom11.js
+++ 05_DOM/js/dom11.js
@@ -4,11 +4,23 @@ const bg = document.getElementById("bg");
 for (let i = 0; i < btn.length; i++) {
     btn[i].onclick = (evt) => {
         console.log(btn)
-        bg.backgroundColor = #tomato;
+        bg = evt.target.backgroundColor(#000);
     }
 }
 
 
 // btn.onclick = () => {
 //     document.body.style.backgroundColor = "tomato";
+// }
+
+// output.innerText = evt.target.innerText + "が押された";
+
+
+// vaScriptソース
+//     ?
+//     1
+// 2
+// 3
+// function changeBoxColor(newColor) {
+//     document.getElementById('sakurabox').style.backgroundColor = newColor;
 // }
