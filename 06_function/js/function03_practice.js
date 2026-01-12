c251e3edd93b137720f5e7fcb57dce212a11742c
towa0105 <2250290@ecc.ac.jp>
2026-01-12 16:45:01 +0900

    追加：function03完了

--- 06_function/js/function03_practice.js
+++ 06_function/js/function03_practice.js
@@ -2,10 +2,10 @@ const msg = document.getElementById("text");
 const btn = document.getElementById("btn");
 const out = document.getElementById("out");
 
-function message(val){
-
+function message(val) {
+    out.innerText = (`あたなの入力した文字は${msg.value}です。`)
 }
 
 btn.onclick = () => {
-  
+    message();
 }
